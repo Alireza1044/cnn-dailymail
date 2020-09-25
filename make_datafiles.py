65fe7df71bbf180988a45ab40ef38e20e3153399
Alireza Moradi <34605487+Alireza1044@users.noreply.github.com>
2020-09-25 20:15:22 +0330

    Update make_datafiles.py

--- make_datafiles.py
+++ make_datafiles.py
@@ -16,9 +16,9 @@ END_TOKENS = ['.', '!', '?', '...', "'", "`", '"', dm_single_close_quote, dm_dou
 SENTENCE_START = '<s>'
 SENTENCE_END = '</s>'
 
-all_train_urls = "/content/drive/My Drive/Internship/cnn-dailymail/url_lists/all_train.txt"
-all_val_urls = "/content/drive/My Drive/Internship/cnn-dailymail/url_lists/all_val.txt"
-all_test_urls = "/content/drive/My Drive/Internship/cnn-dailymail/url_lists/all_test.txt"
+all_train_urls = "/content/drive/url_lists/all_train.txt"
+all_val_urls = "/content/drive/url_lists/all_val.txt"
+all_test_urls = "/content/drive/url_lists/all_test.txt"
 
 cnn_tokenized_stories_dir = "cnn_stories_tokenized"
 dm_tokenized_stories_dir = "dm_stories_tokenized"
